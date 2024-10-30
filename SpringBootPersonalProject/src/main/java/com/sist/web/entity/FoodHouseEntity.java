581806a645fe02635353ab89de3d9d53f1d8799e
1zeen2 <dnjswls6878@naver.com>
2024-10-30 18:26:46 +0900

    Start

---
+++ SpringBootPersonalProject/src/main/java/com/sist/web/entity/FoodHouseEntity.java
@@ -0,0 +1,32 @@
+package com.sist.web.entity;
+/*
+ * 	FNO int 
+	NAME text 
+	TYPE text 
+	PHONE text 
+	ADDRESS text 
+	SCORE double 
+	THEME text 
+	POSTER text 
+	IMAGES text 
+	TIME text 
+	PARKING text 
+	CONTENT text 
+	RDAYS text 
+	JJIMCOUNT int 
+	LIKECOUNT int 
+	HIT int 
+	REPLYCOUNT int
+ */
+
+import java.util.*;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "food_house")
+
+public interface FoodHouseEntity {
+//	private int fno;
+//	private int jjimcount, likecount, replycount;
+//	private String 
+}
